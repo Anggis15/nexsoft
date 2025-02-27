2c5b8e0b13c6805604e56f23ea19e6a81bc7c209
anggis234 <anggis234@gmail.com>
2025-02-27 20:07:49 +0700

    revisi setelah dinilai

--- login.js
+++ login.js
@@ -2,14 +2,14 @@ function main(e){
     const forgotPasswordLink = document.getElementById('forgotPasswordLink');
     const loginForm = document.getElementById('loginForm');
     const passwordFields = document.getElementById('passwordFields');
-    const actionBtn = document.querySelector('.action-btn');
+    const loginBtn = document.querySelector('.action-btn');
     const textLogin = document.getElementById('text-login')
 
     forgotPasswordLink.addEventListener('click', function(event) {
         event.preventDefault();
         loginForm.style.display = 'none';
         passwordFields.style.display = 'block'; 
-        actionBtn.textContent = 'Login'; 
+        loginBtn.textContent = 'Login'; 
         forgotPasswordLink.style.display = 'none'; 
         textLogin.style.display = 'none';
     });
@@ -50,7 +50,7 @@ function main(e){
 
     document.getElementById('resetButton').addEventListener('click', function(event) {
         event.preventDefault();
-        const userId = document.getElementById('userId').value;
+        const userId = document.getElementById('userIdReset').value;
         const currentPassword = document.getElementById('currentPassword').value;
         const newPassword = document.getElementById('newPassword').value;
         const confirmPassword = document.getElementById('confirmPassword').value;
@@ -61,33 +61,34 @@ function main(e){
             loginForm.style.display = 'block';
             forgotPasswordLink.style.display = 'block';
             textLogin.style.display = '';
-        }
-        clearInputFields(["userId", "currentPassword", "newPassword", "confirmPassword"]);
-        let uri = `/nexdist/userchangepassword?action=save&userID=${clearInputFields(userId)}&password=${clearInputFields(currentPassword)}&newPassword=${clearInputFields(newPassword)}`
+        }else{
+            clearInputFields(["userIdReset", "currentPassword", "newPassword", "confirmPassword"]);
+        let uri = `/nexdist/userchangepassword?action=save&userID=${sanitizeInput(userId)}&password=${sanitizeInput(currentPassword)}&newPassword=${sanitizeInput(newPassword)}`
         let responseData = fetch(uri).then(response => {
             if (!response.ok) {
-                showError("Gagal mereset password")
+                alert("Gagal mereset password!!!");
+                
+            }else if(response.ok){
+                showError('Password reset successfully!');
             }
             })
             .then(data => {
                 return data.json()
             })
             .catch(error => {
-                showError("Gagal mereset password")
+                alert("Server error")
             });
         
-        showError('Password reset successfully!');
             passwordFields.style.display = 'none';
             loginForm.style.display = 'block';
             forgotPasswordLink.style.display = 'block';
             textLogin.style.display = '';
+        }
+        
     });
-
     
-    clearInputFields()
-    
-    actionBtn.addEventListener('click', function() {
-        if (actionBtn.textContent === 'Login') {
+    loginBtn.addEventListener('click', function() {
+        if (loginBtn.textContent === 'Login') {
             passwordFields.style.display = 'none';
             loginForm.style.display = 'block';
             forgotPasswordLink.style.display = 'block';
@@ -100,23 +101,15 @@ document.addEventListener("DOMContentLoaded", main)
 
 
 
-function validatePasswordFields(userID, currentPassword, newPassword, confirmPassword) {
+function validatePasswordFields(userId, currentPassword, newPassword, confirmPassword) {
     let messages = [];
 
-    if (!userID)
+    if (!userId)
         messages.push("Harap isi user ID");
     if (!currentPassword)
         messages.push("Harap isi password saat ini");
-    if (!checkRegex(newPassword, /^.{8,}$/))
-        messages.push("Kata sandi setidaknya memiliki 8 karakter");
-    if (!checkRegex(newPassword, /^(?=.*?[A-Z])/))
-        messages.push("Kata sandi setidaknya memiliki 1 huruf besar");
-    if (!checkRegex(newPassword, /^(?=.*?[0-9])/))
-        messages.push("Kata sandi setidaknya memiliki 1 angka");
-    if (!checkRegex(newPassword, /^(?=.*?[!@#$%&*])/))
-        messages.push("Kata sandi setidaknya memiliki 1 special character");
-    if (checkRegex(newPassword, /(\s)/))
-        messages.push("Kata sandi tidak boleh mengandung spasi");
+    if (!checkRegex(newPassword, /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[_#\-$])(\s).{8,16}$/))
+        messages.push("Kata sandi setidaknya memiliki 8 karakterFormat minimal 1 angka, 1 huruf kecil, 1 huruf besar, 1 spesial karakter, tanpa spasi min 8 max 16 alfanumerik, contoh : aB4$12345");
     if (newPassword !== confirmPassword)
         messages.push("Sandi Baru dan Sandi Konfirmasi harus sama");
     if (newPassword === currentPassword)
